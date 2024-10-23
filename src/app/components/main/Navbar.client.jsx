275f2f529402681fcb24e4d280e36ae54f543abf
Anselm Powell <anselm.powell@sport.wales>
2024-10-23 21:26:42 +0100

    add design system fix nav

--- src/app/components/main/Navbar.client.jsx
+++ src/app/components/main/Navbar.client.jsx
@@ -42,12 +42,6 @@ export default function Navbar() {
 
               {/* Navigation Links */}
               <div className="flex space-x-8">
-                <Link
-                  href="/"
-                  className={`p-2 text-md transition-fast ${isActivePath('/')}`}
-                >
-                  Home
-                </Link>
                 <div className="flex items-center space-x-4">
                 <button
                   onClick={() => openAuthModal('login')}
