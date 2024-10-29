ac527c120504efb2f989328e8130c26f79a1afe8
Anselm Powell <anselm.powell@sport.wales>
2024-10-29 19:01:33 +0000

    add url check real env update

--- src/app/api/backend/check/route.js
+++ src/app/api/backend/check/route.js
@@ -5,7 +5,7 @@ export const DJANGO_BASE_URL=config.backendBaseUrl
 
 export async function GET() {
       console.log("URL---------------", DJANGO_BASE_URL)
-      const data = {endpoint: DJANGO_BASE_URL}
+      const data = {endpoint: process.env.BACKEND_BASE_URL}
       
       return NextResponse.json(data, {status: 200});
 }
